162e864ac946410e43bb2a5505ee37010be1b79a
hejz <75412985@qq.com>
2022-05-05 09:30:22 +0800

    代码整合mysql和jpa正常启动

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-boot-jpa
 spring boot jpa学习
+
+[学习视频youtube——需要科学上学](https://www.youtube.com/watch?v=8SGI_XS5OPw&t=4s)
---
+++ studay/src/main/java/com/hejz/studay/StudayApplication.java
@@ -0,0 +1,13 @@
+package com.hejz.studay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudayApplication.class, args);
+    }
+
+}
